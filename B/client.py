cf52a44f5505a11160fcf9d1563f68c0643a0a3b
manoel <manoelneto164@gmail.com>
2020-10-17 19:12:44 -0300

    tarefa B, primeiro commit

--- B/client.py
+++ B/client.py
@@ -5,8 +5,8 @@ serverPort = 61000
 clientSocket = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-sentence = input('Digite o texto em letras minusculas: ')
+sentence = input('Digite o comando: ')
 clientSocket.send(sentence.encode('utf-8'))
-modifiedSentence = clientSocket.recv(1024)
+modifiedSentence = clientSocket.recv(4096)
 print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
 clientSocket.close()
